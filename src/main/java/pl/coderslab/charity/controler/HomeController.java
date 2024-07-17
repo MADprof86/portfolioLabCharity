e79441365ec1ea7680ef4fa40f800c11ac2d4a24
Krzysztof Zarzycki <krzys.zarzycki@gmail.com>
2024-07-17 22:13:31 +0200

    Initial commit

---
+++ src/main/java/pl/coderslab/charity/controler/HomeController.java
@@ -0,0 +1,14 @@
+package pl.coderslab.charity.controler;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+
+public class HomeController {
+    @GetMapping("/")
+    public String getHomePage(Model model){
+        return "index";
+    }
+}
