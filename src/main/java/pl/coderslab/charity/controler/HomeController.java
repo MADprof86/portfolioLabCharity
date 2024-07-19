b6010d033d0b6347838bcb19e5229a4f24a3f540
Krzysztof Zarzycki <krzys.zarzycki@gmail.com>
2024-07-19 17:38:48 +0200

    Added list of all deonations to the HomeService + passed the necessary atributes to the site.

--- src/main/java/pl/coderslab/charity/controler/HomeController.java
+++ src/main/java/pl/coderslab/charity/controler/HomeController.java
@@ -4,7 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import pl.coderslab.charity.model.Donation;
 import pl.coderslab.charity.model.Institution;
+import pl.coderslab.charity.repository.DonationRepository;
 import pl.coderslab.charity.repository.InstitutionRepository;
 
 import java.util.List;
@@ -15,10 +17,19 @@ import java.util.Optional;
 public class HomeController {
     @Autowired
     private InstitutionRepository institutionRepository;
+    @Autowired
+    private DonationRepository donationRepository;
     @GetMapping("/")
     public String getHomePage(Model model){
+        List<Donation> donations = donationRepository.findAll();
+        int donationCount = donations.size();
+        int bagsCount = donations.stream().mapToInt(Donation::getQuantity).sum();
+
+        model.addAttribute("donationCount",donationCount);
+        model.addAttribute("bagsCount",bagsCount);
 
         List<Institution> institutions = institutionRepository.findAll();
+
 
         model.addAttribute("insitutions",institutions);
         return "index";
